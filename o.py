c4cab6d4185e9d917b157f6af3046892be30012f
Capricon1 <133960924+Capricon1@users.noreply.github.com>
2024-01-02 12:15:12 +0800

    20240102

--- o.py
+++ o.py
@@ -1,9 +1,10 @@
-import ctypes
-import os
-import requests
+import ctypes, os, requests
 from tkinter import messagebox
 
 class BingAPI:
+
+    def __init__(self):
+        self.image_url = self.get_url()
 
     def get_url(self):
         base_url = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
@@ -13,26 +14,22 @@ class BingAPI:
         else: full_url = ''
         return full_url
 
-    def process_download(self, image_url):
-        if image_url != '':
-            response = requests.get(image_url)
+    def process_download(self):
+        if self.image_url != '':
+            response = requests.get(self.image_url)
             if response.status_code == 200:
-                with open('th.jpg', 'wb') as image_file:
+                with open('0.jpg', 'wb') as image_file:
                     image_file.write(response.content)
                 return True
         return False
 
-    def set_wallpaper(self):
+def set_wallpaper():
+    ret = BingAPI().process_download()
+    if ret:
         base_path = os.getcwd()
-        ctypes.windll.user32.SystemParametersInfoW(20, 0, f"{base_path}\\th.jpg", 0)
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, f"{base_path}\\0.jpg", 0)
+        messagebox.showinfo('Done', ':)')
+    else:
+        messagebox.showerror('Error', 'An error occurred, try agin.')
 
-    def run(self):
-        url = self.get_url()
-        ret = self.process_download(url)
-        if ret == True:
-            self.set_wallpaper()
-            messagebox.showinfo('Done', ':)')
-        else:
-            messagebox.showerror('Error', 'An error occurred')
-
-BingAPI().run()
+set_wallpaper()
