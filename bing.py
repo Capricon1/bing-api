3c82fb62d1e2844e7b0a10315cdd95b34505d0c1
Capricon1 <133960924+Capricon1@users.noreply.github.com>
2024-01-02 12:22:13 +0800

    更新 bing.py

--- bing.py
+++ bing.py
@@ -18,7 +18,7 @@ class BingAPI:
         if self.image_url != '':
             response = requests.get(self.image_url)
             if response.status_code == 200:
-                with open('0.jpg', 'wb') as image_file:
+                with open('out.jpg', 'wb') as image_file:
                     image_file.write(response.content)
                 return True
         return False
@@ -27,7 +27,7 @@ def set_wallpaper():
     ret = BingAPI().process_download()
     if ret:
         base_path = os.getcwd()
-        ctypes.windll.user32.SystemParametersInfoW(20, 0, f"{base_path}\\0.jpg", 0)
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, f"{base_path}\\out.jpg", 0)
         messagebox.showinfo('Done', ':)')
     else:
         messagebox.showerror('Error', 'An error occurred, try agin.')
